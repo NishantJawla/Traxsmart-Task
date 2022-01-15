4707da83308bd2a9321543afee6cb85f5c6e8970
NishantJawla13 <40868641+NishantJawla@users.noreply.github.com>
2022-01-15 15:01:49 +0530

    post delete commit

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,6 @@ const {sendSuccess, sendError} = require("../lib/helper");
 const { BAD_REQUEST } = require("../lib/statuscodes");
 router.post("/create",allAuth,(req,res) => {
     const{content} = req.body;
-    console.log(req.user)
     let post = new Post({
         content,
         owner: req.user.id
@@ -16,5 +15,27 @@ router.post("/create",allAuth,(req,res) => {
     } catch (err){
         sendError(res,"Post Creation Failed","BAD_REQUEST")
     }
+})
+router.get("/delete/:postid",allAuth,(req,res) => {
+    const postid = req.params.postid;
+    Post.findById(postid,async (err,post) => {
+        if(!post){
+            return sendError(res,"Post Not Available",BAD_REQUEST);
+        }
+        else if(post.owner.toString() != req.user.id.toString()){
+            return sendError(res,"Owner can only delete the post",BAD_REQUEST);
+        } else {
+            try {
+                await Post.remove({
+                    _id: postid
+                });
+                return sendSuccess(res,"Post Deleted")
+            }
+            catch(err) {
+                return sendError(res,"Some error occured while deleting post..", BAD_REQUEST)
+            }
+
+        }
+    })
 })
 module.exports = router;
